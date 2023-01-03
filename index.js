9c87ca35dd9dca6838108e47430caeef8b9ee162
shraddha-3 <onlypeaceinlife@gmail.com>
2023-01-03 14:31:02 +0530

    Add part 3.9-3.11 phonebook-backend step 9-11

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
-var morgan = require('morgan')
 const app = express()
-
+const cors = require('cors')
 
 let persons = [
     { 
@@ -25,18 +24,14 @@ let persons = [
       "number": "39-23-6423122"
     }
 ]
+app.use(cors())
 
 app.use(express.json())
 
-morgan.token('data', function(req, res) {
-    return JSON.stringify(req.body)
-})
-
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
+app.use(express.static('build'))
 
 
 app.get('/api/persons', (request, response) => {
-  const persons = request.body
   response.json(persons)
 })
 
@@ -100,7 +95,7 @@ app.post('/api/persons', (request, response) => {
 })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
